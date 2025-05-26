ff91b0cd98bf28b9718b4eb87eb24fa831b759c2
Nikhil Sharma <Nikhil.sharma1574@gmail.com>
2025-05-26 16:19:23 +0530

    env correction

--- next.config.ts
+++ next.config.ts
@@ -5,4 +5,9 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = {
+  env: {
+    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  },
+}
